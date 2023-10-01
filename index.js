2c16fb4dbe3fee59f943d578ca210c7d1a2cc95b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-01 11:13:21 +0100

    added some changes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// types
+import { typeDefs  } from './schema.js';
+
 // Server Setup
 const server = new ApolloServer({
+    typeDefs,
     // typeDefs --- definitions of types of data
     // resolvers
 });
