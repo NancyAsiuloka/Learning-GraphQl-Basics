1658795f8b87527958173d39ce76aa29bad25043
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-01 11:57:13 +0100

    added a function

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ const resolvers = {
         games(){
             return db.games
         },
+        game(_, args){
+            return db.games.find((game) => game.id === args.id)
+        },
         authors(){
             return db.authors
+        },
+        author(_, args){
+            return db.authors.find((author) => author.id === args.id)
         },
         reviews(){
             return db.reviews
