5a9e5abed83d8c9b481a0e0bd7c825fe8c62efd0
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-01 12:43:43 +0100

    added a resolver for deleteGame

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ const resolvers = {
         game(parent){
             return db.games.find((g) => g.id === parent.game_id)
         }
+    },
+    Mutation: {
+        deleteGame(_, args){
+            db.games = db.games.filter((g) => g.id !== args.id)
+        }
     }
 }
 
